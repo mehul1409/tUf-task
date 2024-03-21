e4386640d442573b6b4047b32642da9a1347f7f4
mehul1409 <mehulbansalswm1234@gmail.com>
2024-03-21 23:04:05 +0530

    credentials change

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "takeUforward"
+    host: "blv5lx0rjnhcru1pyist-mysql.services.clever-cloud.com",
+    user: "ucpwpm8ucuvyhuao",
+    password: "01R6IVcXp8cxHPM7gpxy",
+    database: "blv5lx0rjnhcru1pyist"
 });
 
 module.exports = con;
